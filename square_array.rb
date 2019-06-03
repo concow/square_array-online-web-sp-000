9231b46e5b5ef228d86d2faa481870984d3a6579
Connor Cowan <ccowan003@comcast.net>
2019-06-03 22:33:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   squared_array = []                  #initialize (create) new array in which we use and add to below
-  array.each { |element|              #set pipes to number variable. Do calculations below
+  array.each { |element|              #set pipes = to element variable. Do calculations below
     squared_array << element ** 2     #combine 2 lines of code into one. add new squared numbers (element) to out new array we created above
   }
   return squared_array                #explicitly return new array. if not, it returns original array.
